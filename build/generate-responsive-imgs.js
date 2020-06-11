24bcb7ae80375a2c729ee973a5ac4937f01b9baf
Tim Giles Jr <6314039+TGiles@users.noreply.github.com>
2020-06-11 00:51:07 -0400

    fix: fix responsive image generation

--- build/generate-responsive-imgs.js
+++ build/generate-responsive-imgs.js
@@ -5,37 +5,64 @@ const fs = require('fs');
 const mkdirp = require('mkdirp');
 const gifResize = require('@gumlet/gif-resize');
 
+const webp = require('webp-converter');
 
 const main = () => {
     glob("../assets/**/*.+(png|jpg|jpeg|gif)", async function (err, files) {
         if (err) {
             throw err;
         }
-
-        const widths = new Array(5).fill(0);
-        for (let i = 0; i < widths.length; i++) {
-            widths[i] = 320 + 240 * i;
-        }
+        // These widths come from the images responsiver plugin in .eleventy.js
+        const widths = [250, 488, 725, 963, 1200];
         console.log(`Generating images with the following widths: ${widths}`);
         for (let i = 0; i < files.length; i++) {
             let dir = path.dirname(files[i]);
-            dir = dir.replace("/assets/", "/img/");
+            dir = dir.replace("assets", "img");
             const made = mkdirp.sync(dir);
 
             for (let j = 0; j < widths.length; j++) {
                 const extension = path.extname(files[i]);
                 const base = path.basename(files[i], extension);
-                const outputPath = path.join(dir, `${base}-${widths[j]}${extension}`);
                 if (extension === '.gif') {
+                    const outputPath = path.join(dir, `${base}-${widths[j]}${extension}`);
                     const buffer = fs.readFileSync(files[i]);
                     let transformedGif = await gifResize({
                         width: widths[j]
                     })(buffer);
-                    fs.writeFileSync(outputPath, transformedGif);          
+                    fs.writeFileSync(outputPath, transformedGif);
+                    // TODO: Figure out how to convert gifs to webm
+                    // 
+                    // const gif = fs.readFileSync(outputPath);
+                    // const outputWebm = path.join(dir, `${base}-${widths[j]}.webm`);
+                    // webp.gwebp(outputPath, outputWebm, "-q 80", function (status, error) {
+                    //     console.log(status, error);
+                    //     fs.unlinkSync(outputPath);
+                    // });
+
                 } else {
-                    await sharp(files[i])
-                        .resize(widths[j])
-                        .toFile(outputPath);
+                    const outputPath = path.join(dir, `${base}-${widths[j]}.webp`);
+                    if (base.includes('favicon')) {
+                        if (widths[j] === 250) {
+                            await sharp(files[i])
+                                .resize(32)
+                                .toFile(path.join(dir, `${base}${extension}`));
+                            break;
+                        }
+                    }
+                    else if (base.includes('profile')) {
+                        if (widths[j] === 250) {
+                            await sharp(files[i])
+                                .resize(widths[j])
+                                .webp()
+                                .toFile(outputPath);
+                            break;
+                        }
+                    } else {
+                        await sharp(files[i])
+                            .resize(widths[j])
+                            .webp()
+                            .toFile(outputPath);
+                    }
 
                 }
 
